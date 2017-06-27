03bb09384f5f4ec33ba6bf063141c7993dbf3d66
Cameron Cabo <ccabo@seas.upenn.edu>
2017-06-27 12:36:16 -0700

    Added location routes within dungles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, path: 'users', path_names: { sign_in: 'login', sign_out: 'logout', password: 'password', confirmation: 'confirmation', unlock: 'unlock', registration: 'register', sign_up: 'signup' }
   resources :users, except: [:new] do
-    resources :dungles
+    resources :dungles do
+      resources :location, except: [:index, :show]
+    end
   end
   root 'welcome#index'
 end
