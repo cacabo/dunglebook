2c112f6a5ce04ef8f8aa8cd348027d4817243776
Cameron Cabo <ccabo@seas.upenn.edu>
2017-06-28 16:41:17 -0700

    dropped the locations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628232841) do
+ActiveRecord::Schema.define(version: 20170628233941) do
 
   create_table "dungles", force: :cascade do |t|
     t.integer "user_id"
@@ -27,17 +27,6 @@ ActiveRecord::Schema.define(version: 20170628232841) do
     t.float "longitude"
     t.float "latitude"
     t.index ["user_id"], name: "index_dungles_on_user_id"
-  end
-
-  create_table "locations", force: :cascade do |t|
-    t.integer "dungle_id"
-    t.string "title"
-    t.string "address"
-    t.float "longitude"
-    t.float "latitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["dungle_id"], name: "index_locations_on_dungle_id"
   end
 
   create_table "users", force: :cascade do |t|
