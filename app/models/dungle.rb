9ee0972276b1efe29695ed46c65c0cf7d30ce847
Cameron Cabo <ccabo@seas.upenn.edu>
2017-06-27 17:06:11 -0700

    added validation for location

--- app/models/dungle.rb
+++ app/models/dungle.rb
@@ -8,9 +8,19 @@ class Dungle < ApplicationRecord
   validates :cleanliness, presence: true
   validates :traffic, presence: true
 
+  validate :location
+
   validates_inclusion_of :performance, :in => 1..5
   validates_inclusion_of :ambience, :in => 1..5
   validates_inclusion_of :scent, :in => 1..5
   validates_inclusion_of :cleanliness, :in => 1..5
   validates_inclusion_of :traffic, :in => 1..5
+
+  private
+
+  def location
+    if location_title.blank? and address.blank?
+      errors.add(:base, 'Dungle must have location title or address')
+    end
+  end
 end
