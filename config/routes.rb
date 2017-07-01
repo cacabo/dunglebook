c09bf7549a7efffdf504c25f27132a973a8eee74
Cameron Cabo <ccabo@seas.upenn.edu>
2017-07-01 13:29:34 -0700

    fixed navbar issues

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
     path: 'users',
     path_names: { sign_in: 'login', sign_out: 'logout', password: 'password', confirmation: 'confirmation', unlock: 'unlock', registration: 'register', sign_up: 'signup' },
     controllers: { registrations: 'registrations'}
+
   resources :users, except: [:new] do
     resources :dungles
   end
+  
   root 'welcome#index'
 end
